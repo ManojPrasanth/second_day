f3e27ae82cbdec985bbd7303177db476ede275a0
ManojPrasanth <manojprasanth.siva@gmail.com>
2016-09-02 19:23:01 +0530

    Add files via upload

---
+++ Evennum.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Evennum
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int num=s.nextInt();
+int num1=s.nextInt();
+for(int i=num;i<=num1;i++)
+{
+if(i%2==0)
+{
+System.out.print(" "+i);
+}
+} 
+
+}
+}
